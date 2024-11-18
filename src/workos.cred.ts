1b6e5e818b340f2bdc720a211add830b29ce7197
jagotranaveen <36670920+jagotranaveen@users.noreply.github.com>
2024-11-18 22:54:13 +0530

    Update workos.cred.ts

--- src/workos.cred.ts
+++ src/workos.cred.ts
@@ -1,7 +1,7 @@
 // Abhinav cred
-export const WORKOS_API_KEY='sk_test_a2V5XzAxSkI5Sjc5R1RFSEhGOUUwOVlRUkhZSEMxLGZDdEhzTFphRjF3RU9HY1g5TVdtWDFOS0c'
-export const WORKOS_CLIENT_ID='client_01JB9J7A43RR2MSEA4HZTB476M'
-// const WORKOS_COOKIE_PASSWORD="DS3cJ8o9YaP1ybWfd2evT0Kk+92wOM2Lf3AzRrK6awvtLa7zqIu+BuJBEB2nwdeWZ8oOnOw2MYlZIYnBXdcdGw=="
+export const WORKOS_API_KEY=process.env.WORKOS_API_KEY;
+export const WORKOS_CLIENT_ID=process.env.WORKOS_CLIENT_ID;
+// const WORKOS_COOKIE_PASSWORD=process.env.WORKOS_COOKIE_PASSWORD;
 
 // //  configured in the WorkOS dashboard
 // const NEXT_PUBLIC_WORKOS_REDIRECT_URI="http://localhost:3000/api/identity/auth0-callback"

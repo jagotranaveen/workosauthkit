1f5c5f4a73ae7738498abc7251a5e44f04470be9
jagotranaveen <36670920+jagotranaveen@users.noreply.github.com>
2024-11-28 18:12:57 +0530

    Update workos.cred.ts

--- src/workos.cred.ts
+++ src/workos.cred.ts
@@ -1,6 +1,6 @@
 // Abhinav cred
-export const WORKOS_API_KEY=process.env.WORKOS_API_KEY
-export const WORKOS_CLIENT_ID=process.env.WORKOS_CLIENT_ID
+export const WORKOS_API_KEY=APIKEY!!!!!
+export const WORKOS_CLIENT_ID=CLIENT)ID00001
 //  const WORKOS_COOKIE_PASSWORD=process.env.WORKOS_COOKIE_PASSWORD
 
 // //  configured in the WorkOS dashboard
